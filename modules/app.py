cb4f4ada3d126f194669e724634fb7cc5d5523e8
sambapython@gmail.com <sambapython@gmail.com>
2019-06-18 08:06:15 +0530

    2019-06-18 python file as a module

---
+++ modules/app.py
@@ -0,0 +1,14 @@
+#customer_create()
+'''
+import sales
+print(sales.x, sales.y, sales.z)
+sales.customer_create()
+'''
+'''
+import sales as s1
+print(s1.x, s1.y, s1.z)
+s1.customer_create()
+'''
+#import pur
+#import sys
+#import pur
---
+++ modules/sales.py
@@ -0,0 +1,10 @@
+x=1000
+y=2000
+z=x+y
+a=10
+b=20
+c=30
+def customer_create():
+	print("customer created successfully!!")
+customer_create()
+print(f"x={x}, y={y}, z={z}")
